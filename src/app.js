d278b54b9882acbc5395bee8074cca54e907bada
Gapur <110934827+Albogachiev@users.noreply.github.com>
2024-10-08 20:36:53 +0300

    написал структуру таблиц и связи между ним в файле plan.md подключил end express eslint

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+const { PORT } = process.env;
+
+
+app.listen(PORT, () => console.log(`Сервер крутится на ${PORT} порту!`));
